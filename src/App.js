478e71e70a8899f1a7a49c41e2f134313d7fac4f
kinshukshah <kinshukshah@gmail.com>
2021-04-16 23:54:03 +0530

    Added Base Route Setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import Navbar from './components/navbar/navbar.component';
-import Homepage from './Pages/homepage/homepage.component';
+import "./App.css";
+import Navbar from "./components/navbar/navbar.component";
+import Homepage from "./Pages/homepage/homepage.component";
+
+import { Switch, Route, Link } from "react-router-dom";
+import ShopPage from "./Pages/shoppage/shop.component";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Homepage/>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route exact path="/shop">
+          <ShopPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/Pages/shoppage/shop.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./shop.styles.css";
+
+const ShopPage = () => {
+  return <div>Shop Page</div>;
+};
+
+export default ShopPage;
