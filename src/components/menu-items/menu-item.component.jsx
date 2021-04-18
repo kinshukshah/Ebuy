69a070a3e278bf1b045edc1065253986df5d47d0
kinshukshah <kinshukshah@gmail.com>
2021-04-18 18:09:32 +0530

    Added AddToCart Button

--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import "./menu-item.styles.css";
 import ProdImage5 from "../../assets/product-5.jpg";
+import { Link } from "react-router-dom";
 const MenuItem = ({ id, name, imageUrl, price }) => {
   return (
     <div className="col-4">
       <img src={imageUrl} alt=""></img>
-      <h4>{name}</h4>
-      <div className="rating">
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="far fa-star"></i>
+      <div className="menu-footer">
+        <h4>{name}</h4>
+        <div className="rating">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="far fa-star"></i>
+        </div>
+        <p>${price}</p>
       </div>
-      <p>${price}</p>
+      <button className="addtocart">Add to Cart</button>
     </div>
   );
 };
